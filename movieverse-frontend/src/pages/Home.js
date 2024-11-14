1824a6d3b9e2c2d65c1ac084c28654c9db937911
Congying Zhao <t3cozh00@students.oamk.fi>
2024-11-14 14:46:21 +0200

    link to select page

--- movieverse-frontend/src/pages/Home.js
+++ movieverse-frontend/src/pages/Home.js
@@ -31,7 +31,7 @@ export default function Home() {
 
   return (
     <div>
-      <SectionTitle title="MOVIES" linkPath="/more-movies" />
+      <SectionTitle title="MOVIES" linkPath="/select-movies" />
       <div>
         <MovieList movies={movies} />
       </div>
