88272a75635895bad08a511f25494793744095af
likpik <anna.k04@wp.pl>
2024-11-18 20:55:11 +0200

    Database update

--- movieverse-backend/movieverse.sql
+++ movieverse-backend/movieverse.sql
@@ -1,9 +1,41 @@
+CREATE TABLE account (
+    id       SERIAL PRIMARY KEY,
+    email    VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    status   CHAR(1) NOT NULL,
+    link     VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE favourite (
+    account_id INTEGER NOT NULL,
+    movie_id   INTEGER NOT NULL,
+    PRIMARY KEY (account_id, movie_id),
+    CONSTRAINT favourite_account_fk FOREIGN KEY (account_id) REFERENCES account(id)
+);
+
+CREATE TABLE movie_group (
+    id          SERIAL PRIMARY KEY,
+    name        VARCHAR(50) NOT NULL,
+    owner_id    INTEGER NOT NULL,
+    description VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE group_member (
+    account_id    INTEGER NOT NULL,
+    group_id      INTEGER NOT NULL,
+    member_status VARCHAR(50) NOT NULL,
+    PRIMARY KEY (account_id, group_id),
+    CONSTRAINT group_member_account_fk FOREIGN KEY (account_id) REFERENCES account(id),
+    CONSTRAINT group_member_group_fk FOREIGN KEY (group_id) REFERENCES movie_group(id)
+);
+
 CREATE TABLE review (
-    id          SERIAL PRIMARY KEY,          
-    title       VARCHAR(100) NOT NULL,       
-    rating      INTEGER NOT NULL,            
-    description VARCHAR(255),                
-    timestamp   TIMESTAMPTZ NOT NULL,        
-    movie_id    INTEGER NOT NULL,            
-    account_id  INTEGER NOT NULL             
+    id          SERIAL PRIMARY KEY,
+    rating      INTEGER NOT NULL,
+    title       VARCHAR(50) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    review_date TIMESTAMP WITH TIME ZONE NOT NULL,
+    account_id  INTEGER,
+    movie_id    INTEGER,
+    CONSTRAINT review_account_fk FOREIGN KEY (account_id) REFERENCES account(id)
 );
