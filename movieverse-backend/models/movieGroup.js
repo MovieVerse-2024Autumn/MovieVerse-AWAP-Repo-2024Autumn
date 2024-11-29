36af44c5386949fd08d8a8641628c88bd5d85cb6
Congying Zhao <t3cozh00@students.oamk.fi>
2024-11-29 09:55:52 +0200

    'More groups' list query update

--- movieverse-backend/models/movieGroup.js
+++ movieverse-backend/models/movieGroup.js
@@ -2,14 +2,37 @@ import { pool } from "../middleware/db.js";
 
 // create new group
 export const createGroup = async (name, description, adminId) => {
-  const query = `
+  const client = await pool.connect();
+  try {
+    // begin transaction
+    await client.query("BEGIN");
+
+    const query = `
     INSERT INTO movie_group (name, description, admin_id) 
     VALUES ($1, $2, $3) 
     RETURNING *;
   `;
-  const values = [name, description, adminId];
-  const result = await pool.query(query, values);
-  return result.rows[0];
+    const values = [name, description, adminId];
+    const result = await client.query(query, values);
+    const newGroup = result.rows[0];
+
+    const memberQuery = `
+    INSERT INTO group_member (account_id, group_id, member_status, admin_id) 
+    VALUES ($1, $2, 'accepted', $3);
+  `;
+    const memberValues = [adminId, newGroup.id, adminId];
+    await client.query(memberQuery, memberValues);
+
+    // commit transaction
+    await client.query("COMMIT");
+    return newGroup;
+  } catch (err) {
+    // rollback transaction
+    await client.query("ROLLBACK");
+    throw err;
+  } finally {
+    client.release();
+  }
 };
 
 // get the groups created by user
